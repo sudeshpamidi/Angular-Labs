76558ad11c65663b574791a0f756552741aabe6b
Sudesh Pamidi <sudesh.pamidi@gmail.com>
2019-09-23 14:51:57 -0400

    Users project created

---
+++ users/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Users';
+
+  
+
+  firstName : string = '';
+  lastName : string = '';
+  email : string = '';
+
+  newUser: string = '';
+  addNewUser:boolean = false;
+  currentYear: number = 2019;
+
+
+  onReset() : void {
+    this.firstName  = '';
+    this.lastName = '';
+    this.email  = '';    
+    this.addNewUser = false;
+  }
+
+  onAddUser(): void {
+    this.newUser = `${this.firstName} - ${this.lastName}': ${this.email}`;
+    this.addNewUser = true;
+  }
+
+  getCurrentYear(): number {
+    return this.currentYear;
+  }
+
+  getColor():string {
+    return  (this.addNewUser === true) ? "green" : "red";
+  }
+
+
+}
